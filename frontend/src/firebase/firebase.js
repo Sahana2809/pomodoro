b0a95ca5dde2a8b32d76ba01d9b8a5f368ad1545
Sahana V Agadi <sahanavagadi@gmai.com>
2025-12-04 05:56:55 +0530

    firebase js updated

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfZTPmbPN3CVwzE-eKHcQ2gyFJDakL60w",
-  authDomain: "pomodorabbit.firebaseapp.com",
-  projectId: "pomodorabbit",
-  storageBucket: "pomodorabbit.firebasestorage.app",
-  messagingSenderId: "482507412766",
-  appId: "1:482507412766:web:6e6e298a478ed2f162650f",
-  measurementId: "G-9D3HBGXV2H"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+  
 
 const app = initializeApp(firebaseConfig);
 
